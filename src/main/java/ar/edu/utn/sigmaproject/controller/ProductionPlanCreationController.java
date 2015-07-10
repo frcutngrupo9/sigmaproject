789f0286700669025f2dd7e3cf022650010390ea
Daniel <50815@sistemas.frc.utn.edu.ar>
2015-07-09 22:05:29 -0300

    Agregado funcionalidad pantalla creacion plan de produccion

--- src/main/java/ar/edu/utn/sigmaproject/controller/ProductionPlanCreationController.java
+++ src/main/java/ar/edu/utn/sigmaproject/controller/ProductionPlanCreationController.java
@@ -1,15 +1,19 @@
 package ar.edu.utn.sigmaproject.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Bandbox;
+import org.zkoss.zul.Intbox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Button;
 
-import ar.edu.utn.sigmaproject.domain.Piece;
-import ar.edu.utn.sigmaproject.domain.Process;
 import ar.edu.utn.sigmaproject.domain.Product;
 import ar.edu.utn.sigmaproject.domain.ProductionPlan;
 import ar.edu.utn.sigmaproject.domain.ProductionPlanDetail;
@@ -25,6 +29,14 @@ public class ProductionPlanCreationController extends SelectorComposer<Component
 	
 	@Wire
     Listbox productPopupListbox;
+	@Wire
+    Listbox productionPlanProductListbox;
+	@Wire
+	Bandbox productBandbox;
+	@Wire
+	Intbox productUnits;
+	@Wire
+	Button addProductButton;
 	
 	// services
 	ProductService productListService = new ProductServiceImpl();
@@ -32,22 +44,56 @@ public class ProductionPlanCreationController extends SelectorComposer<Component
 	ProductionPlanDetailService productionPlanDetailService = new ProductionPlanDetailServiceImpl();
 	
 	ListModelList<Product> productListModel;
-    ListModelList<ProductionPlan> productionPlanListModel;
     ListModelList<ProductionPlanDetail> productionPlanDetailListModel;
     
     // atributes
-    private ProductionPlan productionPlan;
-	private ProductionPlanDetail productionPlanDetail;
-	private List<ProductionPlan> productionPlanList;
+    private Product selectedProduct;
+    private List<Product> productList;
 	private List<ProductionPlanDetail> productionPlanDetailList;
 	
 	@Override
     public void doAfterCompose(Component comp) throws Exception {
         super.doAfterCompose(comp);
-        List<Product> productList = productListService.getProductList();
+        productList = productListService.getProductList();
         productListModel = new ListModelList<Product>(productList);
-        productPopupListbox.setModel(productionPlanListModel);
+        productPopupListbox.setModel(productListModel);
+        
+        selectedProduct = null;
+        productionPlanDetailList = new ArrayList<ProductionPlanDetail>();
+        productionPlanDetailListModel = new ListModelList<ProductionPlanDetail>(productionPlanDetailList);
+        productionPlanProductListbox.setModel(productionPlanDetailListModel);
     }
-
+	
+	@Listen("onSelect = #productPopupListbox")
+    public void selectionProductPopupListbox() {
+		selectedProduct = (Product) productPopupListbox.getSelectedItem().getValue();
+		productBandbox.setValue(selectedProduct.getName());
+		productBandbox.close();
+    }
+	
+	@Listen("onClick = #addProductButton")
+    public void addProductToProdPlan() {
+		if(productUnits.getValue()==null || productUnits.getValue().intValue()<=0){
+			Clients.showNotification("Ingresar Cantidad del Producto",productUnits);
+			return;
+		}
+		productListModel.remove(selectedProduct);
+		productPopupListbox.setModel(productListModel);
+		ProductionPlanDetail aux = new ProductionPlanDetail(null,selectedProduct.getId(),productUnits.getValue());
+		productionPlanDetailList.add(aux);
+		productionPlanDetailListModel.add(aux);
+		productionPlanProductListbox.setModel(productionPlanDetailListModel);
+		
+		// luego de actualizar la tabla borramos el text del producto  seleccionado
+		// deseleccionamos la tabla y borramos la cantidad
+		productBandbox.setValue("");
+		productPopupListbox.clearSelection();
+		productUnits.setText(null);
+    }
+	
+	public String getNameOfProduct(int idProduct) {
+		Product aux = productListService.getProduct(idProduct);
+		return aux.getName();
+    }
 	
 }
---
+++ src/main/java/ar/edu/utn/sigmaproject/controller/ProductionPlanListController.java
@@ -0,0 +1,37 @@
+package ar.edu.utn.sigmaproject.controller;
+
+import java.util.List;
+
+import org.zkoss.zul.ListModelList;
+
+import ar.edu.utn.sigmaproject.domain.Product;
+import ar.edu.utn.sigmaproject.domain.ProductionPlan;
+import ar.edu.utn.sigmaproject.domain.ProductionPlanDetail;
+import ar.edu.utn.sigmaproject.service.ProductService;
+import ar.edu.utn.sigmaproject.service.ProductionPlanDetailService;
+import ar.edu.utn.sigmaproject.service.ProductionPlanService;
+import ar.edu.utn.sigmaproject.service.impl.ProductServiceImpl;
+import ar.edu.utn.sigmaproject.service.impl.ProductionPlanDetailServiceImpl;
+import ar.edu.utn.sigmaproject.service.impl.ProductionPlanServiceImpl;
+
+public class ProductionPlanListController {
+	
+	// services
+	ProductService productListService = new ProductServiceImpl();
+	ProductionPlanService productionPlanService = new ProductionPlanServiceImpl();
+	ProductionPlanDetailService productionPlanDetailService = new ProductionPlanDetailServiceImpl();
+	
+	ListModelList<Product> productListModel;
+    ListModelList<ProductionPlan> productionPlanListModel;
+    ListModelList<ProductionPlanDetail> productionPlanDetailListModel;
+    
+    // atributes
+    private Product selectedProduct;
+    private List<Product> productList;
+    private ProductionPlan productionPlan;
+	private ProductionPlanDetail productionPlanDetail;
+	private List<ProductionPlan> productionPlanList;
+	private List<ProductionPlanDetail> productionPlanDetailList;
+	
+
+}
