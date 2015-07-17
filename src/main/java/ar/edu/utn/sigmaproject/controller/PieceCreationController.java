e3d4972b58b7594b917d37a6de9fa28629daa273
Daniel Helmfelt <danielrh1986@gmail.com>
2015-07-17 00:43:23 -0300

    Eliminado cosas no utilizadas

--- src/main/java/ar/edu/utn/sigmaproject/controller/PieceCreationController.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.utn.sigmaproject.controller;
-
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.select.SelectorComposer;
-
-public class PieceCreationController extends SelectorComposer<Component>{
-	private static final long serialVersionUID = 1L;
-	
-}
