85447601e5691b30cdf0d443d33b6a7ba57e4814
DanielRH <50815@sistemas.frc.utn.edu.ar>
2015-12-24 01:57:41 -0300

    agregado creacion de pedido de autoabastecimiento a la pantalla de stock

--- src/main/java/ar/edu/utn/sigmaproject/controller/ProductStockController.java
+++ src/main/java/ar/edu/utn/sigmaproject/controller/ProductStockController.java
@@ -1,25 +1,30 @@
 package ar.edu.utn.sigmaproject.controller;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import org.zkoss.lang.Strings;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Button;
+import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Grid;
 import org.zkoss.zul.Intbox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Textbox;
 
+import ar.edu.utn.sigmaproject.domain.Order;
+import ar.edu.utn.sigmaproject.domain.OrderDetail;
 import ar.edu.utn.sigmaproject.domain.Product;
 import ar.edu.utn.sigmaproject.domain.ProductExistence;
-import ar.edu.utn.sigmaproject.domain.SupplyType;
+import ar.edu.utn.sigmaproject.service.ClientService;
 import ar.edu.utn.sigmaproject.service.ProductExistenceService;
 import ar.edu.utn.sigmaproject.service.ProductService;
+import ar.edu.utn.sigmaproject.service.impl.ClientServiceImpl;
 import ar.edu.utn.sigmaproject.service.impl.ProductExistenceServiceImpl;
 import ar.edu.utn.sigmaproject.service.impl.ProductServiceImpl;
 
@@ -30,8 +35,6 @@ public class ProductStockController extends SelectorComposer<Component>{
     Textbox searchTextbox;
 	@Wire
     Listbox productListbox;
-	@Wire
-    Button newButton;
 	@Wire
     Grid productExistenceGrid;
 	@Wire
@@ -50,20 +53,34 @@ public class ProductStockController extends SelectorComposer<Component>{
     Button cancelButton;
     @Wire
     Button resetButton;
+    @Wire
+    Button newProvisionOrderButton;
+    @Wire
+	Component orderCreationBlock;
+    @Wire
+    Grid orderDetailGrid;
+    @Wire
+    Intbox orderNumber;
+    @Wire
+    Datebox orderNeedDateBox;
     
     // services
     private ProductExistenceService productExistenceService = new ProductExistenceServiceImpl();
     private ProductService productService = new ProductServiceImpl();
+    private ClientService clientService = new ClientServiceImpl();
     
     // atributes
     private ProductExistence currentProductExistence;
     private Product currentProduct;
+    private Order currentOrder;
     
     // list
     private List<Product> productList;
+    private List<OrderDetail> orderDetailList;
     
     // list models
     private ListModelList<Product> productListModel;
+    private ListModelList<OrderDetail> orderDetailListModel;
 	
 	@Override
     public void doAfterCompose(Component comp) throws Exception{
@@ -73,8 +90,10 @@ public class ProductStockController extends SelectorComposer<Component>{
         productListModel = new ListModelList<Product>(productList);
         productListbox.setModel(productListModel);
         currentProduct = null;
-        
         refreshView();
+        
+        currentOrder = null;
+        refreshViewOrder();
     }
 	
 	@Listen("onClick = #searchButton")
@@ -120,7 +139,7 @@ public class ProductStockController extends SelectorComposer<Component>{
 			saveButton.setDisabled(true);
 			cancelButton.setDisabled(true);
 			resetButton.setDisabled(true);
-			newButton.setDisabled(false);
+			newProvisionOrderButton.setDisabled(false);
 			currentProductExistence = null;
 		}else {// editando o creando
 			productExistenceGrid.setVisible(true);
@@ -143,7 +162,7 @@ public class ProductStockController extends SelectorComposer<Component>{
 			saveButton.setDisabled(false);
 			cancelButton.setDisabled(false);
 			resetButton.setDisabled(false);
-			newButton.setDisabled(true);
+			newProvisionOrderButton.setDisabled(true);
 		}
     }
 	
@@ -192,4 +211,69 @@ public class ProductStockController extends SelectorComposer<Component>{
 		}
     	return value + "";
     }
+	
+	// creacion de pedido de auto abastecimiento
+	@Listen("onClick = #newProvisionOrderButton")
+    public void newProvisionOrder() {
+		Integer order_client_id = null;// deberia ser el id del cliente auto abastecimiento
+		Integer order_number = null;
+		Date order_date = new Date();
+		Date order_need_date = null;
+		currentOrder = new Order(null, order_client_id, order_number, order_date, order_need_date);
+		
+		refreshViewOrder();
+    }
+	
+	@Listen("onClick = #resetOrderButton")
+    public void resetOrder() {
+		refreshViewOrder();
+    }
+	
+	@Listen("onClick = #cancelOrderButton")
+    public void cancelOrder() {
+		currentOrder = null;
+		refreshViewOrder();
+    }
+	
+	@Listen("onClick = #saveOrderButton")
+    public void saveOrder() {
+		for(OrderDetail each:orderDetailList) {
+			System.out.println("prod: " + each.getIdProduct() + ", units: " + each.getUnits());
+			// por lo visto no se modifican los elementos de la lista
+		}
+    }
+	
+	private void refreshViewOrder() {
+		if(currentOrder == null) {
+			orderCreationBlock.setVisible(false);
+			orderDetailList = null;
+		} else {
+			orderCreationBlock.setVisible(true);
+			orderNumber.setValue(currentOrder.getNumber());
+			orderNeedDateBox.setValue(null);
+			
+			orderDetailList = new ArrayList<OrderDetail>();
+			List<ProductExistence> list_product_existence_complete = productExistenceService.getProductExistenceList();
+			for(ProductExistence each:list_product_existence_complete) {
+				int units_to_repo = each.getStockRepo() - each.getStock();// si esta resta da un valor positivo quiere decir que el valor de reposicion esta por arriba del stock, por lo tanto ese valor es el necesario
+				if(units_to_repo > 0) {
+					orderDetailList.add(new OrderDetail(null, each.getIdProduct(), units_to_repo, new BigDecimal("0")));
+				}
+			}
+	        orderDetailListModel = new ListModelList<OrderDetail>(orderDetailList);
+	        orderDetailGrid.setModel(orderDetailListModel);
+		}
+		
+	}
+	
+	public String getProductName(int idProduct) {
+		Product aux = productService.getProduct(idProduct);
+		return aux.getName();
+    }
+	
+	public String getProductCode(int idProduct) {
+		Product aux = productService.getProduct(idProduct);
+		return aux.getCode();
+    }
+	
 }
