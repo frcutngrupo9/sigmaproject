f0eea6b36a9538f7c93e60a6fb43441bd559380a
Juan Pedro <architectt@gmail.com>
2019-03-06 10:32:15 -0300

    Agregado de menu en base a roles.

--- src/main/java/ar/edu/utn/sigmaproject/controller/SidebarController.java
+++ src/main/java/ar/edu/utn/sigmaproject/controller/SidebarController.java
@@ -26,12 +26,17 @@ package ar.edu.utn.sigmaproject.controller;
 
 import ar.edu.utn.sigmaproject.desktop.SigmaDesktopInit;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
+import ar.edu.utn.sigmaproject.domain.User;
+import ar.edu.utn.sigmaproject.web.Attributes;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.event.MouseEvent;
@@ -52,13 +57,13 @@ import org.zkoss.zul.Treeitem;
 public class SidebarController extends SelectorComposer<Component> {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
 	private static String SIDEBAR_CONTROLLER_PAGE_ATTRIBUTE_NAME = "mainPage";
 
-	@Wire 
+	@Wire
 	private Tree tree;
 
 	public static Page mainPage() {
@@ -70,52 +75,110 @@ public class SidebarController extends SelectorComposer<Component> {
 		super.doAfterCompose(comp);
 
 		Executions.getCurrent().getDesktop().setAttribute(SIDEBAR_CONTROLLER_PAGE_ATTRIBUTE_NAME, getPage());
+		User user = (User)Sessions.getCurrent(true).getAttribute(Attributes.USER_CREDENTIAL);
 
-		TreeNode<MenuTreeRow> rootNode = new DefaultTreeNode<MenuTreeRow>(null, Arrays.asList(
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Productos"), Arrays.asList(
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Productos", "/product_list.zul")),
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Crear Producto", "/product_creation.zul"))
-			)),
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Pedidos"), Arrays.asList(
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Pedidos", "/order_list.zul")),
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Crear Pedido", "/order_creation.zul"))
-			)),
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Produccion"), Arrays.asList(
+		if (user == null) {
+			return;
+		}
+
+		List<DefaultTreeNode<MenuTreeRow>> firstLevelMenus = new ArrayList<>();
+
+		String userTypeAdmin = "Admin";
+		String userTypeGeneralManager = "Gerente General";
+		String userTypeProductionManager = "Jefe de Produccion";
+		String userTypeSalesRepresentative = "Encargado de Ventas";
+		String userTypeAcquisitionsRepresentative = "Encargado de Compras";
+		String userTypePersonalManager = "Jefe de Personal";
+
+		List<DefaultTreeNode<MenuTreeRow>> array;
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeGeneralManager, userTypeProductionManager })) {
+			array = Arrays.asList(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Productos", "/product_list.zul")));
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Crear Producto", "/product_creation.zul")));
+			}
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Productos"), array));
+		}
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeGeneralManager, userTypeSalesRepresentative })) {
+			array = Arrays.asList(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Pedidos", "/order_list.zul")));
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Crear Pedido", "/order_creation.zul")));
+			}
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Pedidos"), array));
+		}
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager })) {
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Produccion"), Arrays.asList(
 					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Avance de Produccion", "/production_follow_up_list.zul")),
 					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Planes de Produccion", "/production_plan_list.zul")),
 					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Crear Plan de Produccion", "/production_plan_creation.zul"))
-			)),
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock"), Arrays.asList(
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock Productos", "/product_stock.zul")),
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock Materia Prima", "/raw_material_stock.zul")),
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock Insumos", "/supply_stock.zul")),
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Maquinas", "/machine_stock.zul"))
-			)),
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Comprobantes de Stock"), Arrays.asList(
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Materia Prima", "/stock_movement_list_wood.zul")),
-				new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Insumos", "/stock_movement_list_supply.zul"))
-			)),
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Pedidos de Materiales"), Arrays.asList(
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Pedidos de Materiales", "/materials_order_list.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Crear Pedido de Materiales", "/materials_order_creation.zul"))
-				)),
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "ABM"), Arrays.asList(
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Clientes", "/client.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Maderas", "/raw_material.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Tipos de Madera", "/wood_type_list.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Insumos", "/supply.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Maquinas", "/machine_list.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Tipos de Maquina", "/machine.zul")),
-					//new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Herramientas", "/tool.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Procesos", "/process.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Costos Mano de Obra", "/work_hour_list.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Empleados", "/worker.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Categorias Producto", "/product_category_list.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Usuarios", "/user_list.zul")),
-					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Ajustes", "/settings_creation.zul"))
-				)),
-			new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Reportes y Estadisticas", "/report.zul"))
-		));
+			)));
+		}
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager, userTypeSalesRepresentative, userTypeAcquisitionsRepresentative })) {
+			array = new ArrayList<>();
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager, userTypeSalesRepresentative })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock Productos", "/product_stock.zul")));
+			}
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager, userTypeAcquisitionsRepresentative })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock Materia Prima", "/raw_material_stock.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock Insumos", "/supply_stock.zul")));
+			}
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Maquinas", "/machine_stock.zul")));
+			}
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Stock"), array));
+		}
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeAcquisitionsRepresentative })) {
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Comprobantes de Stock"), Arrays.asList(
+					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Materia Prima", "/stock_movement_list_wood.zul")),
+					new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Insumos", "/stock_movement_list_supply.zul"))
+			)));
+		}
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeGeneralManager, userTypeAcquisitionsRepresentative })) {
+			array = Arrays.asList(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Listado Pedidos de Materiales", "/materials_order_list.zul")));
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeAcquisitionsRepresentative })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Crear Pedido de Materiales", "/materials_order_creation.zul")));
+			}
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Pedidos de Materiales"), array));
+		}
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeGeneralManager, userTypeProductionManager, userTypeSalesRepresentative, userTypePersonalManager })) {
+			array = new ArrayList<>();
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeGeneralManager, userTypeSalesRepresentative })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Clientes", "/client.zul")));
+			}
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Maderas", "/raw_material.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Tipos de Madera", "/wood_type_list.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Insumos", "/supply.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Maquinas", "/machine_list.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Tipos de Maquina", "/machine.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Procesos", "/process.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Costos Mano de Obra", "/work_hour_list.zul")));
+			}
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager, userTypePersonalManager })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Empleados", "/worker.zul")));
+			}
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeProductionManager })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Categorias Producto", "/product_category_list.zul")));
+			}
+			if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeGeneralManager })) {
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Usuarios", "/user_list.zul")));
+				array.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Ajustes", "/settings_creation.zul")));
+			}
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "ABM"), array));
+		}
+
+		if (user.containsAnyOfTypes(new String[]{ userTypeAdmin, userTypeGeneralManager })) {
+			firstLevelMenus.add(new DefaultTreeNode<MenuTreeRow>(new MenuTreeRow("", "Reportes y Estadisticas", "/report.zul")));
+		}
+
+		TreeNode<MenuTreeRow> rootNode = new DefaultTreeNode<MenuTreeRow>(null, firstLevelMenus);
+
 		tree.setModel(new DefaultTreeModel<MenuTreeRow>(rootNode));
 		tree.addEventListener(Events.ON_CLICK, new EventListener<MouseEvent>() {
 			public void onEvent(MouseEvent event) throws Exception {
@@ -143,7 +206,7 @@ public class SidebarController extends SelectorComposer<Component> {
 					}
 					if (!treeNode.isLeaf()) {
 						treeitem.setOpen(!treeitem.isOpen());
-					}					
+					}
 				}
 			}
 
